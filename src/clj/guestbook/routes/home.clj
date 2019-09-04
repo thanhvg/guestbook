3f75002b594c6b0c68513d7a570aa2211fd42ec1
Thanh Vuong <thanhvg@gmail.com>
2019-09-04 00:16:56 -0600

    01

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -4,10 +4,38 @@
     [guestbook.db.core :as db]
     [clojure.java.io :as io]
     [guestbook.middleware :as middleware]
-    [ring.util.http-response :as response]))
+    [ring.util.http-response :as response]
+    [struct.core :as st]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+(def message-schema
+  [[:name
+    st/required
+    st/string]
+
+   [:message
+    st/required
+    st/string
+    {:message "message must contain at least 10 characters"
+     :validate #(> (count %) 9)}]])
+
+(defn validate-message [params]
+  (first (st/validate params message-schema)))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+       (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
+
+(defn home-page [{:keys [flash] :as request}]
+  (layout/render
+   request
+   "home.html"
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
@@ -16,6 +44,9 @@
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
-   ["/" {:get home-page}]
+   ["/" {:get home-page
+         :post save-message!}]
    ["/about" {:get about-page}]])
+
+
 
--- test/clj/guestbook/test/db/core.clj
+++ test/clj/guestbook/test/db/core.clj
@@ -1,11 +1,11 @@
 (ns guestbook.test.db.core
   (:require
-    [guestbook.db.core :refer [*db*] :as db]
-    [luminus-migrations.core :as migrations]
-    [clojure.test :refer :all]
-    [clojure.java.jdbc :as jdbc]
-    [guestbook.config :refer [env]]
-    [mount.core :as mount]))
+   [guestbook.db.core :refer [*db*] :as db]
+   [luminus-migrations.core :as migrations]
+   [clojure.test :refer :all]
+   [clojure.java.jdbc :as jdbc]
+   [guestbook.config :refer [env]]
+   [mount.core :as mount]))
 
 (use-fixtures
   :once
@@ -16,22 +16,20 @@
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-(deftest test-users
+(deftest test-message
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
-    (is (= 1 (db/create-user!
-               t-conn
-               {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
-                :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
-           (db/get-user t-conn {:id "1"})))))
+    (let [timestamp (java.time.LocalDateTime/now)]
+      (is (= 1 (db/save-message!
+                t-conn
+                {:name "Bob"
+                 :message "Hello, World"
+                 :timestamp timestamp}
+                {:connection t-conn})))
+      (is (=
+           {:name "Bob"
+            :message "Hello, World"
+            :timestamp timestamp}
+           (-> (db/get-messages t-conn {})
+               (first)
+               (select-keys [:name :message :timestamp])))))))
