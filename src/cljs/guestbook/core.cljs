8ec6eec1304ab8aeefab13b92a8629a103dd9e20
Thanh Vuong <thanhvg@gmail.com>
2019-10-30 01:09:43 -0600

    wiwipp

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -1,10 +1,44 @@
-(ns guestbook.core)
+(ns guestbook.core
+  (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :refer [GET POST]]))
+
+
+;; (defn mount-components []
+;;   (let [content (js/document.getElementById "app")]
+;;     (while (.hasChildNodes content)
+;;       (.removeChild content (.-lastChild content)))
+;;     (.appendChild content (js/document.createTextNode "Making repl to spacemacs is hard: so i don't feel the hype at all"))))
+
+(defn message-form []
+  (let [fields (atom {})]
+    (fn []
+      [:div.content
+       [:div.form-group
+        [:p "name: " (:name @fields)]
+        [:p "message: " (:message @fields)]
+        [:p "Name: "
+         [:input.form-control
+          {:type :text
+           :name :name
+           :on-change #(swap! fields assoc :name (-> % .-target .-value))
+           :value (:name @fields)}]]]
+       [:p "Message:"
+        [:textarea.form-control
+         {:rows 4
+          :cols 50
+          :name :message
+          :on-change #(swap! fields assoc :message (-> % .-target .-value))}
+         (:message @fields)]]
+       [:input.btn.btn-primary {:type :submit :value "comment"}]])))
+
+(defn home []
+    [message-form])
 
 (defn mount-components []
-  (let [content (js/document.getElementById "app")]
-    (while (.hasChildNodes content)
-      (.removeChild content (.-lastChild content)))
-    (.appendChild content (js/document.createTextNode "Making repl to spacemacs is hard"))))
+  (reagent/render
+   [home]
+   (js/document.getElementById "app")))
 
 (defn init! []
   (mount-components))
+
