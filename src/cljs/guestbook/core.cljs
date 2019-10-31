444a9d881c9bc172ad9c97020957e4bf98f223eb
Thanh Vuong <thanhvg@gmail.com>
2019-10-30 23:52:25 -0600

    wip

--- src/cljs/guestbook/core.cljs
+++ src/cljs/guestbook/core.cljs
@@ -9,20 +9,28 @@
 ;;       (.removeChild content (.-lastChild content)))
 ;;     (.appendChild content (js/document.createTextNode "Making repl to spacemacs is hard: so i don't feel the hype at all"))))
 
-(defn send-message! [fields]
+(defn send-message! [fields errors]
   (POST "/add-message"
         {:params @fields
          :format :json
          :headers {"Accept" "application/transit+json"
                    "x-csrf-token" (.-csrfToken js/window)}
          :handler #(.log js/console (str "response:" %))
-         :error-handler #(.error js/console (str "error:" %))}))
+         :error-handler #(do (.error js/console (str "error:" %))
+                             (reset! errors (get-in % [:response :errors])))}))
+
+(defn errors-component [errors id]
+  (when-let [error (id @errors)]
+    [:div.alert.alert-danger (clojure.string/join error)]))
 
 (defn message-form []
-  (let [fields (atom {})]
+  (let [fields (atom {})
+        errors (atom nil)]
     (fn []
       [:div.content
+       [errors-component errors :server-error]
        [:div.form-group
+        [errors-component errors :name]
         [:p "name: " (:name @fields)]
         [:p "message: " (:message @fields)]
         [:p "Name: "
@@ -31,6 +39,7 @@
            :name :name
            :on-change #(swap! fields assoc :name (-> % .-target .-value))
            :value (:name @fields)}]]]
+       [errors-component errors :message]
        [:p "Message:"
         [:textarea.form-control
          {:rows 4
@@ -40,11 +49,11 @@
          (:message @fields)]]
        [:input.btn.btn-primary
         {:type :submit
-         :on-click #(send-message! fields)
+         :on-click #(send-message! fields errors)
          :value "comment"}]])))
 
 (defn home []
-    [message-form])
+  [message-form])
 
 (defn mount-components []
   (reagent/render
@@ -53,4 +62,3 @@
 
 (defn init! []
   (mount-components))
-
